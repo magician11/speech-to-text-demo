ae8865ba6a2aeb925c0b6347c3acf5fc2d7c81bf
Andrew Golightly <support@andrewgolightly.com>
2017-12-06 07:25:13 +1300

    using material ui for layout

--- src/components/SpeechToTextDemo.js
+++ src/components/SpeechToTextDemo.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
+import Paper from 'material-ui/Paper';
+import Typography from 'material-ui/Typography';
+import { withStyles } from 'material-ui/styles';
+import Grid from 'material-ui/Grid';
+import Button from 'material-ui/Button';
 import SpeechToText from 'speech-to-text';
 
-import '../styling/main.css';
+const styles = theme => ({
+  root: {
+    paddingTop: 65
+  },
+  paper: theme.mixins.gutters({
+    paddingTop: 22,
+    paddingBottom: 22
+  })
+});
 
 class SpeechToTextDemo extends Component {
   constructor(props) {
@@ -16,14 +29,17 @@ class SpeechToTextDemo extends Component {
 
   componentDidMount() {
     const onAnythingSaid = text => {
+      console.log('heard text', text);
       this.setState({ interimText: text });
     };
 
     const onFinalised = text => {
+      console.log('finalised text', text);
       this.setState({ finalisedText: this.state.finalisedText.concat(text) });
     };
 
     const onFinishedListening = () => {
+      console.log('finished listening..');
       this.setState({ listening: false });
     };
 
@@ -33,35 +49,102 @@ class SpeechToTextDemo extends Component {
         onFinalised,
         onFinishedListening
       );
-      this.listener.startListening();
-      this.setState({ listening: true });
+      this.startListening();
     } catch (error) {
       this.setState({ error: error.message });
     }
   }
+
+  startListening = () => {
+    this.listener.startListening();
+    this.setState({ listening: true });
+  };
 
   render() {
     const { error, interimText, finalisedText, listening } = this.state;
 
     let content;
     if (error) {
-      content = <h1>{error}</h1>;
-    } else {
       content = (
-        <div>
-          <h1>{listening ? 'Listening...' : 'Finished listening.'}</h1>
-          <h2>Current utterances</h2>
-          <p>{interimText}</p>
-          <h2>Finalised text</h2>
-          <ul>
-            {finalisedText.map((str, index) => <li key={index}>{str}</li>)}
-          </ul>
-        </div>
+        <Paper className={this.props.classes.paper}>
+          <Typography type="headline" gutterBottom>
+            {error}
+          </Typography>
+        </Paper>
+      );
+    } else {
+      let buttonForListening;
+
+      if (listening) {
+        buttonForListening = (
+          <Button
+            raised
+            color="primary"
+            onClick={() => this.listener.stopListening()}
+          >
+            Stop Listening
+          </Button>
+        );
+      } else {
+        buttonForListening = (
+          <Button raised color="primary" onClick={() => this.startListening()}>
+            Start Listening
+          </Button>
+        );
+      }
+      content = (
+        <Grid container>
+          <Grid item xs={12} sm={5}>
+            <Paper className={this.props.classes.paper}>
+              <Typography type="headline" gutterBottom>
+                Status: {listening ? 'listening...' : 'finished listening'}
+              </Typography>
+              {buttonForListening}
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={7}>
+            <Paper className={this.props.classes.paper}>
+              <Typography type="headline" gutterBottom>
+                Current utterances
+              </Typography>
+              <Typography gutterBottom>{interimText}</Typography>
+              <Typography type="headline" gutterBottom>
+                Finalised text
+              </Typography>
+              <Typography gutterBottom>
+                <ul>
+                  {finalisedText.map((str, index) => (
+                    <li key={index}>{str}</li>
+                  ))}
+                </ul>
+              </Typography>
+            </Paper>
+          </Grid>
+        </Grid>
       );
     }
 
-    return <div className="speech-to-text-demo">{content}</div>;
+    return (
+      <Grid container justify="center" className={this.props.classes.root}>
+        <Grid item xs={8}>
+          <Grid container>
+            <Grid item xs={12}>
+              <Typography type="display3" gutterBottom>
+                Speech To Text Demo
+              </Typography>
+              <Typography type="subheading" gutterBottom>
+                This is a demo for the{' '}
+                <a href="https://www.npmjs.com/package/speech-to-text">
+                  speech-to-text module on npm
+                </a>.
+              </Typography>
+            </Grid>
+          </Grid>
+          {content}
+        </Grid>
+      </Grid>
+    );
   }
 }
 
-export default SpeechToTextDemo;
+export default withStyles(styles)(SpeechToTextDemo);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'typeface-roboto';
 import SpeechToTextDemo from './components/SpeechToTextDemo';
-
-// remove margins
-// document.body.style.margin = 0;
 
 ReactDOM.render(<SpeechToTextDemo />, document.getElementById('app'));
